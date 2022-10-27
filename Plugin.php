486c9f4619418735a2ff727383a3138bcdcdf690
Chauncey McAskill <chauncey@mcaskill.ca>
2022-10-27 10:46:48 -0400

    Fix issues raised by PHPCS
    Changed:
    - Updated PHPCS ruleset to exclude rules about missing tags and comments which would add little extra information.
    - Sorted XML attributes to ensure namespaces are first.

--- Plugin.php
+++ Plugin.php
@@ -2,39 +2,39 @@
 
 namespace PsalmWordpress;
 
-use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Stmt\Return_;
-use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Stmt\Echo_;
+use Exception;
+use phpDocumentor;
 use PhpParser;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Name;
 use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Echo_;
+use PhpParser\Node\Stmt\Return_;
+use Psalm;
 use Psalm\Codebase;
-use Psalm\Internal\Analyzer\FileAnalyzer;
 use Psalm\CodeLocation;
 use Psalm\Context;
 use Psalm\IssueBuffer;
+use Psalm\Plugin\Hook\AfterEveryFunctionCallAnalysisInterface;
+use Psalm\Plugin\Hook\BeforeFileAnalysisInterface;
+use Psalm\Plugin\Hook\FunctionParamsProviderInterface;
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
-use Psalm\Plugin\Hook\AfterEveryFunctionCallAnalysisInterface;
-use Psalm\Plugin\Hook\FunctionParamsProviderInterface;
-use Psalm\Plugin\Hook\BeforeFileAnalysisInterface;
 use Psalm\StatementsSource;
 use Psalm\Storage\FileStorage;
 use Psalm\Storage\FunctionLikeParameter;
-use SimpleXMLElement;
-use Psalm\Type\Union;
 use Psalm\Type;
-use Psalm;
-use Psalm\Type\TypeNode;
-use Psalm\Type\Atomic\TLiteralString;
-use Psalm\Type\Atomic\TCallable;
-use PhpParser\Node\Arg;
-use PhpParser\Node\Name;
 use Psalm\Type\Atomic;
-use Exception;
+use Psalm\Type\Union;
+use SimpleXMLElement;
 
-class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysisInterface, FunctionParamsProviderInterface, BeforeFileAnalysisInterface {
-
+class Plugin implements
+	AfterEveryFunctionCallAnalysisInterface,
+	BeforeFileAnalysisInterface,
+	FunctionParamsProviderInterface,
+	PluginEntryPointInterface {
 	/**
 	 * @var array<string, array{types: list<Union>}>
 	 */
@@ -43,8 +43,9 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 	public function __invoke( RegistrationInterface $registration, ?SimpleXMLElement $config = null ) : void {
 		$registration->registerHooksFromClass( static::class );
 
-		// if useDefaultStubs is not set or set to anything except false, we want to load the stubs included in this plugin
-		if ( !isset( $config->useDefaultStubs['value'] ) || (string) $config->useDefaultStubs['value'] !== 'false' ) {
+		// If useDefaultStubs is not set or set to anything except false,
+		// we want to load the stubs included in this plugin.
+		if ( ! isset( $config->useDefaultStubs['value'] ) || (string) $config->useDefaultStubs['value'] !== 'false' ) {
 			array_map( [ $registration, 'addStubFile' ], $this->getStubFiles() );
 		}
 
@@ -52,60 +53,59 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 	}
 
 	/**
-	 * resolve a vendor-relative directory-path to the absolute package directory
+	 * Resolves a vendor-relative directory path to the absolute package directory.
 	 *
-	 * the plugin must run both from the source file in the repository (current working directory)
+	 * The plugin must run both from the source file in the repository (current working directory)
 	 * as well as when required as a composer package when the current working directory may not
 	 * have a vendor/ folder and the package directory is detected relative to this file.
 	 *
-	 * @param string $path of a folder, relative, inside vendor/ (composer), must start with 'vendor/' marker
-	 * @return string
+	 * @param string $path Path of a folder, relative, inside `vendor/` (Composer).
+	 *                     Must start with `vendor/` marker.
 	 */
-	private static function getVendorDir(string $path) : string {
+	private static function getVendorDir( string $path ) : string {
 		$vendor = 'vendor/';
 		$self = 'humanmade/psalm-plugin-wordpress';
 
-		if (0 !== strpos($path, $vendor)) {
+		if ( 0 !== strpos( $path, $vendor ) ) {
 			throw new \BadMethodCallException(
-				sprintf('$path must start with "%s", "%s" given', $vendor, $path)
+				sprintf( '$path must start with "%s", "%s" given', $vendor, $path )
 			);
 		}
 
 		$cwd = getcwd();
 
-		// prefer path relative to current working directory (original default)
-		$cwdPath = $cwd . '/' . $path;
-		if (is_dir($cwdPath)) {
-			return $cwdPath;
+		// Prefer path relative to current working directory (original default).
+		$cwd_path = $cwd . '/' . $path;
+		if ( is_dir( $cwd_path ) ) {
+			return $cwd_path;
 		}
 
-		// check running as composer package inside a vendor folder
-		$pkgSelfDir = __DIR__;
-		$vendorDir = dirname($pkgSelfDir, 2);
-		if ($pkgSelfDir === $vendorDir . '/' . $self) {
-			// likely plugin is running as composer package, let's try for the path
-			$pkgPath = substr($path, strlen($vendor));
-			$vendorPath = $vendorDir . '/' . $pkgPath;
-			if (is_dir($vendorPath)) {
-				return $vendorPath;
+		// Check running as composer package inside a vendor folder.
+		$pkg_self_dir = __DIR__;
+		$vendor_dir = dirname( $pkg_self_dir, 2 );
+		if ( $pkg_self_dir === $vendor_dir . '/' . $self ) {
+			// Likely plugin is running as composer package, let's try for the path.
+			$pkg_path = substr( $path, strlen( $vendor ) );
+			$vendor_path = $vendor_dir . '/' . $pkg_path;
+			if ( is_dir( $vendor_path ) ) {
+				return $vendor_path;
 			}
 		}
 
-		// original default behaviour
-		return $cwdPath;
+		// Original default behaviour.
+		return $cwd_path;
 	}
 
 	/**
 	 * @return string[]
 	 */
-	private function getStubFiles(): array {
-
+	private function getStubFiles() : array {
 		return [
-			self::getVendorDir('vendor/php-stubs/wordpress-stubs') . '/wordpress-stubs.php',
-			self::getVendorDir('vendor/php-stubs/wordpress-globals') . '/wordpress-globals.php',
-			self::getVendorDir('vendor/php-stubs/wp-cli-stubs') . '/wp-cli-stubs.php',
-			self::getVendorDir('vendor/php-stubs/wp-cli-stubs') . '/wp-cli-commands-stubs.php',
-			self::getVendorDir('vendor/php-stubs/wp-cli-stubs') . '/wp-cli-i18n-stubs.php',
+			self::getVendorDir( 'vendor/php-stubs/wordpress-stubs' ) . '/wordpress-stubs.php',
+			self::getVendorDir( 'vendor/php-stubs/wordpress-globals' ) . '/wordpress-globals.php',
+			self::getVendorDir( 'vendor/php-stubs/wp-cli-stubs' ) . '/wp-cli-stubs.php',
+			self::getVendorDir( 'vendor/php-stubs/wp-cli-stubs' ) . '/wp-cli-commands-stubs.php',
+			self::getVendorDir( 'vendor/php-stubs/wp-cli-stubs' ) . '/wp-cli-i18n-stubs.php',
 			__DIR__ . '/stubs/overrides.php',
 		];
 	}
@@ -115,19 +115,26 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 			return;
 		}
 
-		$wpHooksDataDir = self::getVendorDir('vendor/johnbillion/wp-hooks/hooks');
+		$wp_hooks_data_dir = self::getVendorDir( 'vendor/johnbillion/wp-hooks/hooks' );
 
-		static::loadHooksFromFile( $wpHooksDataDir . '/actions.json' );
-		static::loadHooksFromFile( $wpHooksDataDir . '/filters.json' );
+		static::loadHooksFromFile( $wp_hooks_data_dir . '/actions.json' );
+		static::loadHooksFromFile( $wp_hooks_data_dir . '/filters.json' );
 	}
 
-	/**
-	 *
-	 * @param string $filepath
-	 * @return array<string, array{ types: list<Union> }>
-	 */
 	protected static function loadHooksFromFile( string $filepath ) : void {
-		/** @var list<array{ name: string, file: string, type: 'action'|'filter', doc: array{ description: string, long_description: string, long_description_html: string, tags: list<array{ name: string, content: string, types?: list<string>}> } }> */
+		/**
+		 * @var list<array{
+		 *     name: string,
+		 *     file: string,
+		 *     type: 'action'|'filter',
+		 *     doc: array{
+		 *         description: string,
+		 *         long_description: string,
+		 *         long_description_html: string,
+		 *         tags: list<array{ name: string, content: string, types?: list<string>}>
+		 *     }
+		 * }>
+		 */
 		$hooks = json_decode( file_get_contents( $filepath ), true );
 		$hook_map = [];
 		foreach ( $hooks['hooks'] as $hook ) {
@@ -139,8 +146,9 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 				if ( isset( $param['types'] ) && $param['types'] !== [ 'array' ] ) {
 					return $param;
 				}
+
 				if ( substr_count( $param['content'], '{' ) !== 1 ) {
-					// Unable to parse nested array style phpdoc.
+					// Unable to parse nested array style PHPDoc.
 					return $param;
 				}
 
@@ -155,7 +163,6 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 				$array_string = 'array{ ' . implode( ', ', $array_properties ) . ' }';
 				$param['types'] = [ $array_string ];
 				return $param;
-
 			}, $params );
 
 			$types = array_column( $params, 'types' );
@@ -164,14 +171,20 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 				return implode( '|', $type );
 			}, $types );
 
-			// remove empty elements which can happen with invalid phpdoc - must be done before parseString to avoid notice there
+			// Remove empty elements which can happen with invalid PHPDoc.
+			// Must be done before parseString to avoid notice there.
 			$types = array_filter( $types );
 
 			static::registerHook( $hook['name'], array_map( [ Type::class, 'parseString' ], $types ), $hook['type'] );
 		}
 	}
 
-	public static function beforeAnalyzeFile( StatementsSource $statements_source, Context $file_context, FileStorage $file_storage, Codebase $codebase ) : void {
+	public static function beforeAnalyzeFile(
+		StatementsSource $statements_source,
+		Context $file_context,
+		FileStorage $file_storage,
+		Codebase $codebase
+	) : void {
 		$statements = $codebase->getStatementsForFile( $statements_source->getFilePath() );
 		$traverser = new PhpParser\NodeTraverser;
 		$hook_visitor = new HookNodeVisitor();
@@ -179,7 +192,7 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 		try {
 			$traverser->traverse( $statements );
 		} catch ( Exception $e ) {
-
+			// Do nothing.
 		}
 
 		foreach ( $hook_visitor->hooks as $hook_name => $hook ) {
@@ -193,7 +206,7 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 		Context $context,
 		StatementsSource $statements_source,
 		Codebase $codebase
-	): void {
+	) : void {
 		$apply_filter_functions = [
 			'apply_filters',
 			'apply_filters_ref_array',
@@ -208,7 +221,7 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 
 		if ( in_array( $function_id, $apply_filter_functions, true ) ) {
 			$hook_type = 'filter';
-		} elseif ( in_array( $function_id, $do_action_functions, true) ) {
+		} elseif ( in_array( $function_id, $do_action_functions, true ) ) {
 			$hook_type = 'action';
 		} else {
 			return;
@@ -280,7 +293,7 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 
 		$hook_name = $call_args[0]->value->value;
 		$hook = static::$hooks[ $hook_name ] ?? null;
-		$is_action = $function_id === 'add_action';
+		$is_action = ( $function_id === 'add_action' );
 
 		if ( ! $hook ) {
 			if ( $code_location ) {
@@ -295,8 +308,8 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 			return [];
 		}
 
-		// action_reference for do_action_ref_array
-		if ( $is_action && !in_array( $hook['hook_type'], array( 'action', 'action_reference' ), true ) ) {
+		// "action_reference" for "do_action_ref_array".
+		if ( $is_action && ! in_array( $hook['hook_type'], [ 'action', 'action_reference' ], true ) ) {
 			if ( $code_location ) {
 				IssueBuffer::accepts(
 					new HookNotFound(
@@ -309,8 +322,8 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 			return [];
 		}
 
-		// filter_reference for apply_filters_ref_array
-		if ( ! $is_action && !in_array( $hook['hook_type'], array( 'filter', 'filter_reference' ), true ) ) {
+		// "filter_reference" for "apply_filters_ref_array".
+		if ( ! $is_action && ! in_array( $hook['hook_type'], [ 'filter', 'filter_reference' ], true ) ) {
 			if ( $code_location ) {
 				IssueBuffer::accepts(
 					new HookNotFound(
@@ -325,7 +338,7 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 
 		// Check how many args the filter is registered with.
 		/** @var int */
-		$num_args = $call_args[ 3 ]->value->value ?? 1;
+		$num_args = $call_args[3]->value->value ?? 1;
 		// Limit the required type params on the hook to match the registered number.
 		$hook_types = array_slice( $hook['types'], 0, $num_args );
 
@@ -339,14 +352,15 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 		} elseif ( isset( $hook['types'][0] ) ) {
 			$return_type = $hook['types'][0];
 		} else {
-			// unknown due to lack of PHPDoc - but a filter must always return something - mixed is the most generic case
+			// Unknown due to lack of PHPDoc - but a filter must always
+			// return something - mixed is the most generic case.
 			$return_type = Type::parseString( 'mixed' );
 		}
 
 		$return = [
 			new FunctionLikeParameter( 'Hook', false, Type::parseString( 'string' ), null, null, false ),
 			new FunctionLikeParameter( 'Callback', false, new Union( [
-				new TCallable(
+				new Atomic\TCallable(
 					'callable',
 					$hook_params,
 					$return_type
@@ -359,28 +373,27 @@ class Plugin implements PluginEntryPointInterface, AfterEveryFunctionCallAnalysi
 	}
 
 	/**
-	 * @param string $hook
 	 * @param list<Union> $types
-	 * @return void
 	 */
-	public static function registerHook( string $hook, array $types, string $hook_type = 'filter' ) {
-		// remove empty elements which can happen with invalid phpdoc
+	public static function registerHook( string $hook, array $types, string $hook_type = 'filter' ) : void {
+		// Remove empty elements which can happen with invalid PHPDoc.
 		$types = array_filter( $types );
 
-		// do not assign empty types if we already have this hook registered
+		// Do not assign empty types if we already have this hook registered.
 		if ( isset( static::$hooks[ $hook ] ) && empty( $types ) ) {
 			return;
 		}
 
-		// if this hook is registered already
+		// If this hook is registered already.
 		if ( isset( static::$hooks[ $hook ] ) ) {
-			// if we have more types than already registered we overwrite existing ones, but keep the additional ones (array_merge would combine them which is wrong)
+			// If we have more types than already registered we overwrite existing ones,
+			// but keep the additional ones (array_merge would combine them which is wrong).
 			$types = $types + static::$hooks[ $hook ]['types'];
 		}
 
 		static::$hooks[ $hook ] = [
 			'hook_type' => $hook_type,
-			'types' => $types,
+			'types'     => $types,
 		];
 	}
 }
@@ -392,7 +405,7 @@ class HookNodeVisitor extends PhpParser\NodeVisitorAbstract {
 	/** @var array<string, list<Union>> */
 	public $hooks = [];
 
-	public function enterNode( PhpParser\Node $origNode ) {
+	public function enterNode( PhpParser\Node $orig_node ) {
 		$apply_filter_functions = [
 			'apply_filters',
 			'apply_filters_ref_array',
@@ -405,35 +418,45 @@ class HookNodeVisitor extends PhpParser\NodeVisitorAbstract {
 			'do_action_deprecated',
 		];
 
-		// "return apply_filters" will assign the phpdoc to the return instead of the apply_filters, so we need to store it
+		// "return apply_filters" will assign the PHPDoc to the return instead
+		// of the apply_filters, so we need to store it
 		// "$var = apply_filters" directly after a function declaration
-		// "echo apply_filters"
-		// cannot do this for all cases, as often it will assign completely wrong stuff otherwise
-		if ( $origNode->getDocComment() && ( $origNode instanceof FuncCall || $origNode instanceof Return_ || $origNode instanceof Variable || $origNode instanceof Echo_ ) ) {
-			$this->last_doc = $origNode->getDocComment();
-		} elseif ( isset( $this->last_doc ) && ! $origNode instanceof FuncCall ) {
-			// if it's set already and this is not a FuncCall, reset it to null, since there's something else and it would be used incorrectly
+		// "echo apply_filters" cannot do this for all cases,
+		// as often it will assign completely wrong stuff otherwise.
+		if (
+			$orig_node->getDocComment() && (
+				$orig_node instanceof FuncCall ||
+				$orig_node instanceof Return_ ||
+				$orig_node instanceof Variable ||
+				$orig_node instanceof Echo_
+			)
+		) {
+			$this->last_doc = $orig_node->getDocComment();
+		} elseif ( isset( $this->last_doc ) && ! $orig_node instanceof FuncCall ) {
+			// If it's set already and this is not a FuncCall, reset it to null,
+			// since there's something else and it would be used incorrectly.
 			$this->last_doc = null;
 		}
 
-		if ( $this->last_doc && $origNode instanceof FuncCall && $origNode->name instanceof Name ) {
-			if ( in_array( (string) $origNode->name, $apply_filter_functions, true ) ) {
+		if ( $this->last_doc && $orig_node instanceof FuncCall && $orig_node->name instanceof Name ) {
+			if ( in_array( (string) $orig_node->name, $apply_filter_functions, true ) ) {
 				$hook_type = 'filter';
-			} elseif ( in_array( (string) $origNode->name, $do_action_functions, true) ) {
+			} elseif ( in_array( (string) $orig_node->name, $do_action_functions, true ) ) {
 				$hook_type = 'action';
 			} else {
 				return null;
 			}
-			if ( ! $origNode->args[0]->value instanceof String_ ) {
+
+			if ( ! $orig_node->args[0]->value instanceof String_ ) {
 				$this->last_doc = null;
 				return null;
 			}
 
-			$hook_name = $origNode->args[0]->value->value;
+			$hook_name = $orig_node->args[0]->value->value;
 
 			$doc_comment = $this->last_doc->getText();
 
-			$doc_factory = \phpDocumentor\Reflection\DocBlockFactory::createInstance();
+			$doc_factory = phpDocumentor\Reflection\DocBlockFactory::createInstance();
 			try {
 				$doc_block = $doc_factory->create( $doc_comment );
 			} catch ( \RuntimeException $e ) {
@@ -442,20 +465,21 @@ class HookNodeVisitor extends PhpParser\NodeVisitorAbstract {
 				return null;
 			}
 
-			/** @var \phpDocumentor\Reflection\DocBlock\Tags\Param[] */
+			/** @var phpDocumentor\Reflection\DocBlock\Tags\Param[] */
 			$params = $doc_block->getTagsByName( 'param' );
 
 			$types = [];
 			foreach ( $params as $param ) {
-				// might be instanceof \phpDocumentor\Reflection\DocBlock\Tags\invalidTag if the param is invalid
-				if( ! ( $param instanceof \phpDocumentor\Reflection\DocBlock\Tags\Param ) ) {
-					// set to mixed - if we skip it, it will mess up all subsequent args
+				// Might be instanceof phpDocumentor\Reflection\DocBlock\Tags\invalidTag
+				// if the param is invalid.
+				if ( ! ( $param instanceof phpDocumentor\Reflection\DocBlock\Tags\Param ) ) {
+					// Set to mixed - if we skip it, it will mess up all subsequent args.
 					$types[] = 'mixed';
 					continue;
 				}
 				$param_type = $param->getType();
 				if ( is_null( $param_type ) ) {
-					// set to mixed - if we skip it, it will mess up all subsequent args
+					// Set to mixed - if we skip it, it will mess up all subsequent args.
 					$types[] = 'mixed';
 					continue;
 				}
@@ -471,7 +495,7 @@ class HookNodeVisitor extends PhpParser\NodeVisitorAbstract {
 
 			$this->hooks[ $hook_name ] = [
 				'hook_type' => $hook_type,
-				'types' => $types,
+				'types'     => $types,
 			];
 			$this->last_doc = null;
 		}
@@ -480,5 +504,4 @@ class HookNodeVisitor extends PhpParser\NodeVisitorAbstract {
 	}
 }
 
-class HookNotFound extends \Psalm\Issue\PluginIssue {}
-
+class HookNotFound extends Psalm\Issue\PluginIssue {}
